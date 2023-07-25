afe1594950ed14161fcaea790e745bb2035c3060
vinnievanilla <vinnie.vanilla@gmail.com>
2023-07-25 23:03:29 +0200

    Update customAgent.py

--- pages/customAgent.py
+++ pages/customAgent.py
@@ -57,24 +57,24 @@ class CustomPromptTemplate(StringPromptTemplate):
 class CustomOutputParser(AgentOutputParser):
 
     def parse(self, llm_output: str) -> Union[AgentAction, AgentFinish]:
-    # Check if agent should finish
-    if "Final Answer:" in llm_output:
-        return AgentFinish(
-            return_values={"output": llm_output.split("Final Answer:")[-1].strip()},
-            log=llm_output,
-        )
-    # Parse out the action and action input
-    regex = r"Action\s*\d*\s*:(.*?)\nAction\s*\d*\s*Input\s*\d*\s*:[\s]*(.*)"
-    match = re.search(regex, llm_output, re.DOTALL)
-    if not match:
-        # Handle case when output doesn't match expected format
-        return AgentFinish(
-            return_values={"output": llm_output},
-            log="Could not parse LLM output, returning as is.",
-        )
-    action = match.group(1).strip()
-    action_input = match.group(2)
-    return AgentAction(tool=action, tool_input=action_input.strip(" ").strip('"'), log=llm_output)
+        # Check if agent should finish
+        if "Final Answer:" in llm_output:
+            return AgentFinish(
+                return_values={"output": llm_output.split("Final Answer:")[-1].strip()},
+                log=llm_output,
+            )
+        # Parse out the action and action input
+        regex = r"Action\s*\d*\s*:(.*?)\nAction\s*\d*\s*Input\s*\d*\s*:[\s]*(.*)"
+        match = re.search(regex, llm_output, re.DOTALL)
+        if not match:
+            # Handle case when output doesn't match expected format
+            return AgentFinish(
+                return_values={"output": llm_output},
+                log="Could not parse LLM output, returning as is.",
+            )
+        action = match.group(1).strip()
+        action_input = match.group(2)
+        return AgentAction(tool=action, tool_input=action_input.strip(" ").strip('"'), log=llm_output)
 
 
 output_parser = CustomOutputParser()
